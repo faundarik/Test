31679ba9c868cec83cf806d2041624925f8ca373
Marco Quezada <Qmarco88@gmail.com>
2015-03-22 17:58:35 -0400

    integrating OpenID into the settings

--- hackathon_starter/hackathon_starter/settings.py
+++ hackathon_starter/hackathon_starter/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hackathon',
+    'django_openid',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -47,6 +48,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_openid_consumer.SessionConsumer',
 )
 
 ROOT_URLCONF = 'hackathon_starter.urls'
